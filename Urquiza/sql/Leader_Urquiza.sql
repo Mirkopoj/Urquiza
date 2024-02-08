8fdfd0fc7ba7931ea58bde4cf601b3f13cde80fb
Mirkopoj <mirkopoj@hotmail.com>
2024-02-08 14:32:48 -0800

    Renames

--- Urquiza/sql/Leader_Urquiza.sql
+++ Urquiza/sql/Leader_Urquiza.sql
@@ -9,7 +9,7 @@
 -------------------------------------	
 INSERT INTO LoadingInfo	
 		(LeaderType,				ForegroundImage,						PlayDawnOfManAudio,		BackgroundImage)
-VALUES	('LEADER_MRK_URQUIZAS',	'LEADER_MRK_URQUIZAS_NEUTRAL.dds',		0,						'LEADER_HOJO_BACKGROUND.dds');	
+VALUES	('LEADER_MRK_URQUIZA',	'LEADER_MRK_URQUIZA_NEUTRAL.dds',		0,						'LEADER_HOJO_BACKGROUND.dds');	
 --==========================================================================================================================
 -- DIPLOMACY INFO
 --==========================================================================================================================
@@ -17,7 +17,7 @@ VALUES	('LEADER_MRK_URQUIZAS',	'LEADER_MRK_URQUIZAS_NEUTRAL.dds',		0,						'LEAD
 -------------------------------------	
 --INSERT INTO DiplomacyInfo	
 --		(Type,						BackgroundImage)
---VALUES	('LEADER_MRK_URQUIZAS',	'FALLBACK_NEUTRAL_MRK_URQUIZAS.dds');
+--VALUES	('LEADER_MRK_URQUIZA',	'FALLBACK_NEUTRAL_MRK_URQUIZA.dds');
 --==========================================================================================================================
 -- COLOURS
 --==========================================================================================================================
@@ -25,12 +25,12 @@ VALUES	('LEADER_MRK_URQUIZAS',	'LEADER_MRK_URQUIZAS_NEUTRAL.dds',		0,						'LEAD
 -------------------------------------	
 INSERT INTO PlayerColors	
 		(Type,					Usage,			PrimaryColor, 								SecondaryColor,								TextColor)
-VALUES	('LEADER_MRK_URQUIZAS','Unique',		'COLOR_PLAYER_MRK_URQUIZAS_BACKGROUND',	'COLOR_PLAYER_MRK_URQUIZAS_FOREGROUND', 	'COLOR_PLAYER_WHITE_TEXT');	
+VALUES	('LEADER_MRK_URQUIZA','Unique',		'COLOR_PLAYER_MRK_URQUIZA_BACKGROUND',	'COLOR_PLAYER_MRK_URQUIZA_FOREGROUND', 	'COLOR_PLAYER_WHITE_TEXT');	
 
 INSERT INTO Colors 
 		(Type, 										Red, 	Green, 	Blue, 	Alpha)
-VALUES	('COLOR_PLAYER_MRK_URQUIZAS_BACKGROUND',	0.24,	0.20,	0.51,	1),
-		('COLOR_PLAYER_MRK_URQUIZAS_FOREGROUND',	0.83,	0.18,	0.13,	1);
+VALUES	('COLOR_PLAYER_MRK_URQUIZA_BACKGROUND',	0.24,	0.20,	0.51,	1),
+		('COLOR_PLAYER_MRK_URQUIZA_FOREGROUND',	0.83,	0.18,	0.13,	1);
 --==========================================================================================================================
 -- LEADERS
 --==========================================================================================================================
@@ -38,33 +38,33 @@ VALUES	('COLOR_PLAYER_MRK_URQUIZAS_BACKGROUND',	0.24,	0.20,	0.51,	1),
 -------------------------------------	
 INSERT INTO Types	
 		(Type,					Kind)
-VALUES	('LEADER_MRK_URQUIZAS','KIND_LEADER');	
+VALUES	('LEADER_MRK_URQUIZA','KIND_LEADER');	
 -------------------------------------
 -- Leaders
 -------------------------------------	
 INSERT INTO Leaders	
 		(LeaderType,					Name,								InheritFrom,		SceneLayers)
-VALUES	('LEADER_MRK_URQUIZAS',		'LOC_LEADER_MRK_URQUIZAS_NAME',	'LEADER_DEFAULT',	4);	
+VALUES	('LEADER_MRK_URQUIZA',		'LOC_LEADER_MRK_URQUIZA_NAME',	'LEADER_DEFAULT',	4);	
 -------------------------------------
 -- LeaderQuotes
 -------------------------------------	
 INSERT INTO LeaderQuotes	
 		(LeaderType,			Quote)
-VALUES	('LEADER_MRK_URQUIZAS','LOC_LEADER_MRK_URQUIZAS_QUOTE');	
+VALUES	('LEADER_MRK_URQUIZA','LOC_LEADER_MRK_URQUIZA_QUOTE');	
 
 -------------------------------------
 -- FavoredReligions
 -------------------------------------	
 INSERT INTO FavoredReligions	
  		(LeaderType,				ReligionType)
-VALUES	('LEADER_MRK_URQUIZAS',	'RELIGION_CATHOLICISM');
+VALUES	('LEADER_MRK_URQUIZA',	'RELIGION_CATHOLICISM');
 
 --==========================================================================================================================
 -- CivilizationLeaders
 --==========================================================================================================================
 INSERT INTO CivilizationLeaders
 		(LeaderType, 			CivilizationType,				CapitalName)
-VALUES  ('LEADER_MRK_URQUIZAS','CIVILIZATION_LEU_ARGENTINA',	'LOC_CITY_NAME_LEU_ARGENTINA_01');
+VALUES  ('LEADER_MRK_URQUIZA','CIVILIZATION_LEU_ARGENTINA',	'LOC_CITY_NAME_LEU_ARGENTINA_01');
 
 --==========================================================================================================================
 --==========================================================================================================================
@@ -77,10 +77,10 @@ VALUES  ('LEADER_MRK_URQUIZAS','CIVILIZATION_LEU_ARGENTINA',	'LOC_CITY_NAME_LEU_
 CREATE TABLE IF NOT EXISTS StartPosition (MapName TEXT, Civilization TEXT, Leader TEXT, X INT default 0, Y INT default 0);
 INSERT INTO StartPosition
 		(Civilization,					Leader,						MapName,			X,		Y)
-VALUES	('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZAS',		'GiantEarth',		160,	14),
-		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZAS',		'GreatestEarthMap',	29,		9),
-		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZAS',		'LargestEarth',		216,	22),
-		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZAS',		'CordiformEarth',	19,		8);
+VALUES	('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZA',		'GiantEarth',		160,	14),
+		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZA',		'GreatestEarthMap',	29,		9),
+		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZA',		'LargestEarth',		216,	22),
+		('CIVILIZATION_LEU_ARGENTINA',	'LEADER_MRK_URQUIZA',		'CordiformEarth',	19,		8);
 
 --==========================================================================================================================
 --=================================
